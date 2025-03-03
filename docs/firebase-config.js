d41970ab0edad07ef11bebcdc229945bca85f281
GraveNews <87426252+gravenewsweekly@users.noreply.github.com>
2025-03-04 03:47:47 +0530

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
-// Firebase Config
+// Firebase Config (Replace with your actual config)
 const firebaseConfig = {
     apiKey: "AIzaSyCQBM-LJabF0jiQeBp0R5RAA0BnGDjZuLY",
     authDomain: "articlex-f2b8f.firebaseapp.com",
     projectId: "articlex-f2b8f",
-    storageBucket: "articlex-f2b8f.appspot.com",
+    storageBucket: "https://gravenewsweekly.github.io/Article-X.com/index.html",
     messagingSenderId: "392539639323",
     appId: "1:392539639323:web:2dfbf35b2b4adfa70ef2be",
     measurementId: "G-5E5TYNZJQ9"
